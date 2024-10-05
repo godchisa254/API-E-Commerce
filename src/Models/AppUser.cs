6583d0945bbfc4085fc77233f764c4240332ec45
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2024-10-05 04:08:59 -0300

    Feat: add model AppUser

---
+++ src/Models/AppUser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace taller1.src.Models
+{
+    public class AppUser : IdentityUser
+    {
+        
+    }
+}
