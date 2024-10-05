1ff9f76ed3938d616f2864dfe03c299fc455c974
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2024-10-05 04:29:11 -0300

    Feat: Identity Authentication and Authentication

--- Program.cs
+++ Program.cs
@@ -1,14 +1,57 @@
+using System.Text;
 using DotNetEnv;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using taller1.src.Data;
+using taller1.src.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Env.Load();
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(
+    opt =>
+    {
+        opt.Password.RequireDigit = false;
+        opt.Password.RequireLowercase = false;
+        opt.Password.RequireUppercase = false;
+        opt.Password.RequireNonAlphanumeric = false;
+        opt.Password.RequiredLength = 8;
+    }
+).AddEntityFrameworkStores<ApplicationDBContext>();
+
+builder.Services.AddAuthentication( 
+    opt =>
+    {
+        opt.DefaultAuthenticateScheme = 
+        opt.DefaultChallengeScheme = 
+        opt.DefaultForbidScheme = 
+        opt.DefaultScheme = 
+        opt.DefaultSignInScheme = 
+        opt.DefaultSignOutScheme = IdentityConstants.ApplicationScheme;
+    }
+).AddJwtBearer(
+    opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER"),
+            ValidateAudience = true,
+            ValidAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_KEY") ?? throw new ArgumentNullException("JWT: SigningKey"))),
+        };
+    }
+);
+
+
 string connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? "Data Source = app.db";
 builder.Services.AddDbContext<ApplicationDBContext>(opt => opt.UseSqlite(connectionString));
 
@@ -22,7 +65,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
+app.UseAuthorization();
 
 
 app.Run();
