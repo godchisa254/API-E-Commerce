4f79d7613521ffd7f9542e797d2cfecd18be404c
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2024-10-05 04:46:22 -0300

    Feat: add AuthController and Register metod

---
+++ src/Controllers/AuthController.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using taller1.src.Dtos;
+using taller1.src.Models;
+
+
+namespace taller1.src.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AuthController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegristationDto registerDto)
+        {
+            try{
+                if(!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+
+                }
+
+                //TODO: crear un exist by code y un exist by email 
+
+                var appUser = new AppUser
+                    {
+                        UserName = registerDto.Name,
+                        Email = registerDto.Email,
+                        Rut = registerDto.Rut,
+                        Birthdate = registerDto.Birthdate,
+                        Gender = registerDto.Gender
+                    };
+
+                if(string.IsNullOrEmpty(registerDto.Password))
+                {
+                    return BadRequest("Password is requerid");
+                }
+
+                var createUser = await _userManager.CreateAsync(appUser, registerDto.Password);
+
+                if(createUser.Succeeded)
+                {
+                    var role = await _userManager.AddToRoleAsync(appUser, "User");
+
+                    if(role.Succeeded)
+                    {
+                        return Ok("User created successfully");
+                    }
+                    else
+                    {
+                        return StatusCode(500, role.Errors);
+                 
+                    }
+
+
+                } else 
+                {
+                    return StatusCode(500, createUser.Errors);
+                }
+
+
+            } catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
--- src/Models/AppUser.cs
+++ src/Models/AppUser.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -8,6 +10,33 @@ namespace taller1.src.Models
 {
     public class AppUser : IdentityUser
     {
-        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; } 
+
+        [ForeignKey("UserRole")]
+        [Required]
+        public int UserRoleID { get; set; } 
+
+        [Required]
+        [StringLength(12)]
+        [Column(TypeName = "varchar")]
+        public string Rut { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Birthdate { get; set; }
+
+        [Required]
+        [Range(0, 4)]
+        public int Gender { get; set; } 
+
+        [Required]
+        [StringLength(20)]
+        [Column(TypeName = "varchar")]
+        public string Password { get; set; } = string.Empty;
     }
 }
