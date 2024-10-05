5edd48252a0784f6c310e1ea88f61cf47660383a
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2024-10-05 04:15:57 -0300

    Chroe: modifiqued ApplicationDBConext Identity

--- src/Data/ApplicationDBContext.cs
+++ src/Data/ApplicationDBContext.cs
@@ -4,18 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using taller1.src.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace taller1.src.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
             
         }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
+
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
         public DbSet<Receipt> Receipts { get; set; }
