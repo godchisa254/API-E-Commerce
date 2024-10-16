1f6de1a21723fafab2b5b0caf2d34bd2668e51bc
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2024-10-16 03:24:08 -0300

    Feat: add NewUserDto

--- src/Interface/ITokenService.cs
+++ src/Interface/ITokenService.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using taller1.src.Models;
 
 namespace taller1.src.Interface
 {
     public interface ITokenService
     {
-        
+        string CreateToken(AppUser user);
     }
 }
