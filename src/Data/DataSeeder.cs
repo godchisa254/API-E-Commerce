2b0cf8e60997a6a1d6a7cfcf6dbd468b21fc661c
godchisa254 <Matias.254@outlook.com>
2024-11-09 21:45:20 -0300

    feat: product types seeder

--- src/Data/DataSeeder.cs
+++ src/Data/DataSeeder.cs
@@ -5,59 +5,61 @@ using System.Threading.Tasks;
 using taller1.src.Models;
 using Bogus;
 
-namespace taller1.src.Data.Migrations
+namespace taller1.src.Data
 {
     public class DataSeeder
     {
-        /**
-        public static void Initialize(IServiceProvider serviceProvider)
+        
+        public static async Task InitializeAsync(IServiceProvider serviceProvider)
         {
             using (var scope = serviceProvider.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 var context = services.GetRequiredService<ApplicationDBContext>();
 
-                if(!context.UserRoles.Any())
-                {
-                    context.UserRoles.AddRange(
-                        new UserRole {Role = "Administrador"},
-                        new UserRole {Role = "Usuario"}
-                    );
+                await SeedProductTypes(context);
 
-                    context.SaveChanges();
-                }
+                // if(!context.UserRoles.Any())
+                // {
+                //     context.UserRoles.AddRange(
+                //         new UserRole {Role = "Administrador"},
+                //         new UserRole {Role = "Usuario"}
+                //     );
+
+                //     context.SaveChanges();
+                // }
 
                 var existingRuts = new HashSet<string>();
 
-                if(!context.Users.Any())
-                {
-                    var userFaker = new Faker<User>()
-                        .RuleFor(u => u.Rut , f => GenerateUniqueRandomRut(existingRuts))
-                        .RuleFor(u => u.Name , f => f.Person.FullName)
-                        .RuleFor(u => u.Birthdate, f => f.Date.Past(30, DateTime.Now.AddYears(-18)))
-                        .RuleFor(u => u.Email , f => f.Person.Email)
-                        .RuleFor(u => u.Gender, f => f.PickRandom(new[] { 0, 1, 2, 3 }))
-                        .RuleFor(u => u.Password, f => f.Internet.Password())
-                        .RuleFor(u => u.UserRoleID , f => f.Random.Number(1,2));
+                // if(!context.Users.Any())
+                // {
+                //     var userFaker = new Faker<User>()
+                //         .RuleFor(u => u.Rut , f => GenerateUniqueRandomRut(existingRuts))
+                //         .RuleFor(u => u.Name , f => f.Person.FullName)
+                //         .RuleFor(u => u.Birthdate, f => f.Date.Past(30, DateTime.Now.AddYears(-18)))
+                //         .RuleFor(u => u.Email , f => f.Person.Email)
+                //         .RuleFor(u => u.Gender, f => f.PickRandom(new[] { 0, 1, 2, 3 }))
+                //         .RuleFor(u => u.Password, f => f.Internet.Password())
+                //         .RuleFor(u => u.UserRoleID , f => f.Random.Number(1,2));
                     
-                    var users = userFaker.Generate(10);
-                    context.Users.AddRange(users);
-                    context.SaveChanges();
-                }
+                //     var users = userFaker.Generate(10);
+                //     context.Users.AddRange(users);
+                //     context.SaveChanges();
+                // }
 
-                if(!context.Products.Any())
-                {
-                    var productFaker = new Faker<Product>()
-                        .RuleFor(p => p.Name , f => f.Commerce.ProductName())
-                        .RuleFor(p => p.ProductTypeID, f => f.PickRandom(new[] { 0, 1, 2, 3, 4 }))
-                        .RuleFor(p => p.Price , f => f.Random.Number(1000, 1000000))
-                        .RuleFor(p => p.Stock, f => f.Random.Number(1, 100))
-                        .RuleFor(p => p.Image, f => f.Image.PicsumUrl());
+                // if(!context.Products.Any())
+                // {
+                //     var productFaker = new Faker<Product>()
+                //         .RuleFor(p => p.Name , f => f.Commerce.ProductName())
+                //         .RuleFor(p => p.ProductTypeID, f => f.PickRandom(new[] { 0, 1, 2, 3, 4 }))
+                //         .RuleFor(p => p.Price , f => f.Random.Number(1000, 1000000))
+                //         .RuleFor(p => p.Stock, f => f.Random.Number(1, 100))
+                //         .RuleFor(p => p.Image, f => f.Image.PicsumUrl());
                     
-                    var products = productFaker.Generate(10);
-                    context.Products.AddRange(products);
-                    context.SaveChanges();
-                }
+                //     var products = productFaker.Generate(10);
+                //     context.Products.AddRange(products);
+                //     context.SaveChanges();
+                // }
 
                 context.SaveChanges();
 
@@ -103,6 +105,31 @@ namespace taller1.src.Data.Migrations
             int verification = 11 - (sum % 11);
             return verification == 11 ? 0 : verification;
         }
-        **/
+
+    public static async Task SeedProductTypes(ApplicationDBContext context)
+    {
+        
+        if(!context.ProductTypes.Any())
+        {
+            var types = new List<ProductType>
+            {
+                new ProductType { Type = "Poleras" },
+                new ProductType { Type = "Gorros" },
+                new ProductType { Type = "Juguetería" },
+                new ProductType { Type = "Alimentación" },
+                new ProductType { Type = "Libros" }
+            };
+
+            foreach (var type in types)
+            {
+                if (!context.ProductTypes.Any(c => c.Type == type.Type))
+                {
+                    context.ProductTypes.Add(type);
+                }
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
     }
 }
