5e68379dcc365aad7bb6cba6ed5484e5c0749669
godchisa254 <Matias.254@outlook.com>
2024-11-17 23:31:14 -0300

    feat: GET all users, admin exclusive functionality

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -18,5 +18,27 @@ namespace taller1.src.Controllers
             _authRepository = authRepository; 
         }
         
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers([FromQuery] QueryUser query)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                var validSortProperties = new[] { "enabledUser", "Name"};
+                if (!validSortProperties.Contains(query.SortBy))
+                {
+                    return BadRequest($"Invalid SortBy property: {query.SortBy}. Use one of the following: {string.Join(", ", validSortProperties)}");
+                }
+            }
+
+            var users = await _authRepository.GetAllUsers(query);
+            var usersDtos = users.Select(u => u.ToGetUserDto()).ToList();
+            return Ok(usersDtos);
+        }
+
     }
 }
