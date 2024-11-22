549e76ce15fc58215240494dd9810a05a7cc574f
godchisa254 <Matias.254@outlook.com>
2024-11-22 02:39:13 -0300

    feat: adds GetReceiptDto

---
+++ src/Dtos/ReceiptDtos/GetReceiptDto.cs
@@ -0,0 +1,15 @@
+namespace taller1.src.Dtos.ReceiptDtos
+{
+    public class GetReceiptDto
+    { 
+        public int ID { get; set; } 
+        public string UserID { get; set; } = string.Empty; 
+        public string Country { get; set; } = string.Empty; 
+        public string City { get; set; } = string.Empty; 
+        public string Commune { get; set; } = string.Empty;  
+        public string Street { get; set; } = string.Empty; 
+        public DateOnly Date { get; set; } 
+        public float Total { get; set; } 
+        
+    }
+}
