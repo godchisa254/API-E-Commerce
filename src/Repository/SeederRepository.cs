0b59679ec8a22447ce6c6fe51299db2590e65bfe
godchisa254 <Matias.254@outlook.com>
2024-11-24 20:18:02 -0300

    doc: adds Seeder Repository and Interface XML documentation in spanish

--- src/Repository/SeederRepository.cs
+++ src/Repository/SeederRepository.cs
@@ -1,35 +1,51 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
-using taller1.src.Data;
 using taller1.src.Interface;
 using taller1.src.Models;
 
 namespace taller1.src.Repository
 {
+    /// <summary>
+    /// Repositorio para el Seeder de la aplicación, gestionando operaciones a ejecutar para una base de datos vacía.
+    /// </summary>
     public class SeederRepository : ISeederRepository
     {
-        private readonly ApplicationDBContext _context;
-
         private readonly UserManager<AppUser> _userManager;
 
-        public SeederRepository(ApplicationDBContext context, UserManager<AppUser> userManager)
+        /// <summary>
+        /// Constructor del repositorio del Seeder.
+        /// </summary>
+        /// <param name="userManager">Gestor de usuarios para manejar operaciones relacionadas con usuarios.</param>
+        public SeederRepository( UserManager<AppUser> userManager)
         {
-            _context = context;
             _userManager = userManager;
         }
+
+        /// <summary>
+        /// Asocia un rol al usuario administrador especificado.
+        /// </summary>
+        /// <param name="admin">El objeto <see cref="AppUser"/> que representa al usuario administrador al que se le asignará un rol.</param>
+        /// <param name="role">El nombre del rol a asignar al usuario administrativo.</param>
+        /// <returns>Devuelve un resultado de identidad <see cref="IdentityResult"/> que indica si la asignación del rol fue exitosa.</returns>
         public async Task<IdentityResult> AddRole(AppUser admin, string role)
         {
             return await _userManager.AddToRoleAsync(admin, role);
         }
 
+        /// <summary>
+        /// Crea un nuevo usuario administrativo en el sistema con el rol de administrador.
+        /// </summary>
+        /// <param name="admin">El objeto <see cref="AppUser"/> que contiene los datos del usuario administrador a crear.</param>
+        /// <param name="password">La contraseña del usuario administrador a crear.</param>
+        /// <returns>Devuelve un resultado de identidad <see cref="IdentityResult"/> que indica si la creación fue exitosa.</returns>
         public async Task<IdentityResult> CreateAdminAsync(AppUser admin, string password)
         {
             return await _userManager.CreateAsync(admin, password);
         }
 
+        /// <summary>
+        /// Obtiene un usuario administrativo basado en su rol.
+        /// </summary>
+        /// <returns>Devuelve el objeto <see cref="AppUser"/> correspondiente al usuario administrativo si existe, de lo contrario devuelve null.</returns>
         public async Task<AppUser?> GetAdminByRol()
         {
             var admin = await _userManager.GetUsersInRoleAsync("Admin");
