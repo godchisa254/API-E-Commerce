b16d306b1766c6e59227526b2d597da4c2fef4e9
godchisa254 <Matias.254@outlook.com>
2024-11-24 19:17:34 -0300

    doc: adds ReceiptDtos XML documentation in spanish

--- src/Dtos/ReceiptDtos/CreateReceiptRequestDto.cs
+++ src/Dtos/ReceiptDtos/CreateReceiptRequestDto.cs
@@ -1,10 +1,29 @@
 namespace taller1.src.Dtos.ReceiptDtos
 {
+    /// <summary>
+    /// DTO utilizado para obtener la dirección del usuario que se agregará a la boleta.
+    /// </summary>
     public class CreateReceiptRequestDto
     {
+
+        /// <summary>
+        /// País asociado a la dirección de la boleta.
+        /// </summary>
         public string Country { get; set; } = string.Empty; 
+
+        /// <summary>
+        /// Ciudad asociada a la dirección de la boleta.
+        /// </summary>
         public string City { get; set; } = string.Empty; 
+
+        /// <summary>
+        /// Comuna asociada a la dirección de la boleta.
+        /// </summary>
         public string Commune { get; set; } = string.Empty;  
+
+        /// <summary>
+        /// Calle asociada a la dirección de la boleta.
+        /// </summary>
         public string Street { get; set; } = string.Empty;
         
     }
